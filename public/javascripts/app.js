ebd28d4ca08722c91f45a031b14d2c4aae8f291d
Harry Hsu <hhsu@mindspring.com>
2015-01-14 00:03:21 +0000

    40-Build a SongPlayer service to share song data across views.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -342,12 +342,11 @@ if (document.URL.match(/\/album.html/)) {
  }]);
 
  // #38 - Angularize Album Page. 1-12-2015 //
- blocJams.controller('Album.controller', ['$scope', function($scope) {
+ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    $scope.album = angular.copy(albumPicasso);
 
    var hoveredSong = null;
-   var playingSong = null;
- 
+    
    $scope.onHoverSong = function(song) {
      hoveredSong = song;
    };
@@ -357,7 +356,7 @@ if (document.URL.match(/\/album.html/)) {
    };
 
    $scope.getSongState = function(song) {
-     if (song === playingSong) {
+     if (song === SongPlayer.currentSong && SongPlayer.playing) {
        return 'playing';
      }
      else if (song === hoveredSong) {
@@ -367,13 +366,42 @@ if (document.URL.match(/\/album.html/)) {
    };
 
    $scope.playSong = function(song) {
-      playingSong = song;
+     SongPlayer.setSong($scope.album, song);
+     SongPlayer.play();
     };
  
     $scope.pauseSong = function(song) {
-      playingSong = null;
+      SongPlayer.pause();
     };
  }]);
+
+ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
+  $scope.songPlayer = SongPlayer;
+}]);
+ 
+ // #40 - Create Song Player Service Tied to Playerbar. 1-13-2015 //
+ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
+   $scope.songPlayer = SongPlayer;
+ }]);
+ 
+ blocJams.service('SongPlayer', function() {
+   return {
+     currentSong: null,
+     currentAlbum: null,
+     playing: false,
+ 
+     play: function() {
+       this.playing = true;
+     },
+     pause: function() {
+       this.playing = false;
+     },
+     setSong: function(album, song) {
+       this.currentAlbum = album;
+       this.currentSong = song;
+     }
+   };
+ });
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
